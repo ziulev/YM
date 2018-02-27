92173783241cc80a04be908ecf4320a39ab5a0ab
Denis Zyulev <zzulev@gmail.com>
2018-02-27 18:41:22 +0700

    add electron-packager; remove semicolon

--- renderer.js
+++ renderer.js
@@ -1,19 +1,19 @@
-const electron = require('electron');
-const ipc      = electron.ipcRenderer;
-const embed    = document.querySelector('.view');
+const electron = require('electron')
+const ipc      = electron.ipcRenderer
+const embed    = document.querySelector('.view')
 
 ipc.on('media-next-track', () => {
-  embed.executeJavaScript(`externalAPI.next()`);
-});
+  embed.executeJavaScript(`externalAPI.next()`)
+})
 
 ipc.on('media-prev-track', () => {
-  embed.executeJavaScript(`externalAPI.prev()`);
-});
+  embed.executeJavaScript(`externalAPI.prev()`)
+})
 
 ipc.on('media-stop', () => {
-  embed.executeJavaScript(`externalAPI.stop()`);
-});
+  embed.executeJavaScript(`externalAPI.stop()`)
+})
 
 ipc.on('media-play-pause', () => {
-  embed.executeJavaScript(`externalAPI.togglePause()`);
-});
+  embed.executeJavaScript(`externalAPI.togglePause()`)
+})
