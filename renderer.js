236c6f0913a9d101d25fd1c22c9255a46ceb5a34
Denis Zyulev <zzulev@gmail.com>
2018-02-28 11:06:32 +0700

    add styles; add screenshot

--- renderer.js
+++ renderer.js
@@ -1,19 +1,31 @@
 const electron = require('electron')
 const ipc      = electron.ipcRenderer
-const embed    = document.querySelector('.view')
+const webview  = document.querySelector('.view')
+
+webview.addEventListener('dom-ready', () => {
+  webview.insertCSS(`
+    .layout_narrow .centerblock-wrapper {
+      margin-right: 0 !important;
+    }
+
+    .bar-below {
+      display: none;
+    }
+  `)
+})
 
 ipc.on('media-next-track', () => {
-  embed.executeJavaScript(`externalAPI.next()`)
+  webview.executeJavaScript(`externalAPI.next()`)
 })
 
 ipc.on('media-prev-track', () => {
-  embed.executeJavaScript(`externalAPI.prev()`)
+  webview.executeJavaScript(`externalAPI.prev()`)
 })
 
 ipc.on('media-stop', () => {
-  embed.executeJavaScript(`externalAPI.stop()`)
+  webview.executeJavaScript(`externalAPI.stop()`)
 })
 
 ipc.on('media-play-pause', () => {
-  embed.executeJavaScript(`externalAPI.togglePause()`)
+  webview.executeJavaScript(`externalAPI.togglePause()`)
 })
